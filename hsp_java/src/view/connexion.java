c5e72db7a4770e97119076df01ad182a99fdd8c0
ryanftne <ryan-fontaine@hotmail.fr>
2021-01-18 17:24:27 +0100

    connexion

--- hsp_java/src/view/connexion.java
+++ hsp_java/src/view/connexion.java
@@ -3,9 +3,13 @@ package view;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
+
+import org.eclipse.swt.graphics.Image;
+
 import javax.swing.JPasswordField;
 
 public class connexion {
@@ -43,29 +47,33 @@ public class connexion {
 	private void initialize() {
 		connexion = new JFrame();
 		connexion.setTitle("Connexion");
-		connexion.setBounds(100, 100, 319, 285);
+		connexion.setBounds(100, 100, 422, 402);
 		connexion.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		connexion.getContentPane().setLayout(null);
 		
-		JLabel lbl_id = new JLabel("Identifiant");
-		lbl_id.setBounds(31, 76, 102, 14);
-		connexion.getContentPane().add(lbl_id);
-		
-		JLabel lbl_mdp = new JLabel("Mot de passe");
-		lbl_mdp.setBounds(31, 155, 102, 14);
-		connexion.getContentPane().add(lbl_mdp);
-		
 		txt_id = new JTextField();
-		txt_id.setBounds(181, 73, 96, 20);
+		txt_id.setBounds(207, 123, 114, 20);
 		connexion.getContentPane().add(txt_id);
 		txt_id.setColumns(10);
 		
 		lblPassword = new JPasswordField();
-		lblPassword.setBounds(181, 152, 96, 17);
+		lblPassword.setBounds(207, 191, 114, 20);
 		connexion.getContentPane().add(lblPassword);
 		
 		JButton btnConnexion = new JButton("Se connecter");
-		btnConnexion.setBounds(86, 200, 127, 23);
+		btnConnexion.setBounds(111, 274, 127, 23);
 		connexion.getContentPane().add(btnConnexion);
+		
+		JLabel label = new JLabel("Identifiant");
+		java.awt.Image img = new ImageIcon(this.getClass().getResource("/face.png")).getImage();
+		label.setIcon(new ImageIcon(img));
+		label.setBounds(89, 105, 166, 56);
+		connexion.getContentPane().add(label);
+		
+		JLabel label2 = new JLabel("Mot de passe");
+		java.awt.Image img1 = new ImageIcon(this.getClass().getResource("/padlock.png")).getImage();
+		label2.setIcon(new ImageIcon(img1));
+		label2.setBounds(89, 173, 166, 56);
+		connexion.getContentPane().add(label2);
 	}
 }
