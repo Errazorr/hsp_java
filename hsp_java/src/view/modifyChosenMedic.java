45988d89fe8d47ea1345ef4564302fca07a8b9d4
Errazorr <trafalgar.nathan@gmail.com>
2021-01-26 14:35:50 +0100

    Page modification médicament

--- hsp_java/src/view/modifyChosenMedic.java
+++ hsp_java/src/view/modifyChosenMedic.java
@@ -4,10 +4,15 @@ import java.awt.EventQueue;
 
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.JTextArea;
 
 public class modifyChosenMedic {
 
 	private JFrame ModifyChosen;
+	private JTextField txtName;
+	private JTextField txtFabriquant;
+	private JTextField txtQty;
 
 	/**
 	 * Launch the application.
@@ -38,28 +43,47 @@ public class modifyChosenMedic {
 	private void initialize() {
 		ModifyChosen = new JFrame();
 		ModifyChosen.setTitle("Modifier m\u00E9dicament");
-		ModifyChosen.setBounds(100, 100, 630, 420);
+		ModifyChosen.setBounds(100, 100, 338, 306);
 		ModifyChosen.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		ModifyChosen.getContentPane().setLayout(null);
 		
-		JLabel lblName = new JLabel("New label");
-		lblName.setBounds(33, 27, 49, 14);
+		JLabel lblName = new JLabel("Nom");
+		lblName.setBounds(63, 58, 54, 14);
 		ModifyChosen.getContentPane().add(lblName);
 		
-		JLabel lblNewLabel_1 = new JLabel("New label");
-		lblNewLabel_1.setBounds(33, 75, 49, 14);
-		ModifyChosen.getContentPane().add(lblNewLabel_1);
+		JLabel lblFabriquant = new JLabel("Fabriquant");
+		lblFabriquant.setBounds(32, 100, 85, 14);
+		ModifyChosen.getContentPane().add(lblFabriquant);
 		
-		JLabel lblNewLabel_2 = new JLabel("New label");
-		lblNewLabel_2.setBounds(33, 122, 49, 14);
-		ModifyChosen.getContentPane().add(lblNewLabel_2);
+		JLabel lblQty = new JLabel("Quantit\u00E9");
+		lblQty.setBounds(43, 141, 74, 14);
+		ModifyChosen.getContentPane().add(lblQty);
 		
-		JLabel lblNewLabel_3 = new JLabel("New label");
-		lblNewLabel_3.setBounds(266, 122, 49, 14);
-		ModifyChosen.getContentPane().add(lblNewLabel_3);
+		JLabel lblLots = new JLabel("Lots de 50");
+		lblLots.setBounds(228, 141, 70, 14);
+		ModifyChosen.getContentPane().add(lblLots);
 		
-		JLabel lblNewLabel_4 = new JLabel("New label");
-		lblNewLabel_4.setBounds(33, 184, 49, 14);
-		ModifyChosen.getContentPane().add(lblNewLabel_4);
+		JLabel lblDangers = new JLabel("Dangers potentiels");
+		lblDangers.setBounds(32, 178, 154, 14);
+		ModifyChosen.getContentPane().add(lblDangers);
+		
+		txtName = new JTextField();
+		txtName.setBounds(110, 55, 96, 20);
+		ModifyChosen.getContentPane().add(txtName);
+		txtName.setColumns(10);
+		
+		txtFabriquant = new JTextField();
+		txtFabriquant.setColumns(10);
+		txtFabriquant.setBounds(110, 94, 96, 20);
+		ModifyChosen.getContentPane().add(txtFabriquant);
+		
+		txtQty = new JTextField();
+		txtQty.setColumns(10);
+		txtQty.setBounds(110, 135, 96, 20);
+		ModifyChosen.getContentPane().add(txtQty);
+		
+		JTextArea txtDangers = new JTextArea();
+		txtDangers.setBounds(32, 203, 240, 22);
+		ModifyChosen.getContentPane().add(txtDangers);
 	}
 }
