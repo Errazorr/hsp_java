0cf7977d2e4a032bc78afea45dcacc9916bc8f20
Errazorr <trafalgar.nathan@gmail.com>
2021-01-26 14:44:41 +0100

    Page de choix de médicament à modifier

--- hsp_java/src/view/modifyMedic.java
+++ hsp_java/src/view/modifyMedic.java
@@ -6,10 +6,14 @@ import javax.swing.JFrame;
 import javax.swing.JTable;
 import javax.swing.JList;
 import javax.swing.JPasswordField;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.JButton;
 
 public class modifyMedic {
 
 	private JFrame Modify;
+	private JTextField textField;
 
 	/**
 	 * Launch the application.
@@ -40,8 +44,21 @@ public class modifyMedic {
 	private void initialize() {
 		Modify = new JFrame();
 		Modify.setTitle("Modifier m\u00E9dicament");
-		Modify.setBounds(100, 100, 714, 417);
+		Modify.setBounds(100, 100, 310, 200);
 		Modify.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		Modify.getContentPane().setLayout(null);
+		
+		JLabel lblName = new JLabel("Nom du m\u00E9dicament \u00E0 modifier");
+		lblName.setBounds(40, 40, 200, 14);
+		Modify.getContentPane().add(lblName);
+		
+		textField = new JTextField();
+		textField.setBounds(40, 65, 200, 20);
+		Modify.getContentPane().add(textField);
+		textField.setColumns(10);
+		
+		JButton btnChoix = new JButton("Modifier");
+		btnChoix.setBounds(100, 115, 89, 23);
+		Modify.getContentPane().add(btnChoix);
 	}
 }
