d0d98d1e8e1e63fea72e3599c0826ffbf9ded675
ryanftne <ryan-fontaine@hotmail.fr>
2021-02-27 14:29:28 +0100

    design rdv

--- hsp_java/src/view/rdv.java
+++ hsp_java/src/view/rdv.java
@@ -51,13 +51,20 @@ public class rdv {
 		Rdv.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		Rdv.getContentPane().setLayout(null);
 		
-		JButton btnNewButton = new JButton("RDV");
+		JButton btnNewButton = new JButton("Voir");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				Rdv.dispose();
+				viewRDV window = new viewRDV();
+				window.frame.setVisible(true);
+			}
+		});
 		btnNewButton.setBackground(Color.DARK_GRAY);
 		btnNewButton.setForeground(Color.WHITE);
-		btnNewButton.setBounds(57, 174, 89, 23);
+		btnNewButton.setBounds(57, 189, 89, 23);
 		Rdv.getContentPane().add(btnNewButton);
 		
-		JButton btnNewButton_1 = new JButton("Patient");
+		JButton btnNewButton_1 = new JButton("Cr\u00E9er");
 		btnNewButton_1.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				Rdv.dispose();
@@ -67,10 +74,10 @@ public class rdv {
 		});
 		btnNewButton_1.setForeground(Color.WHITE);
 		btnNewButton_1.setBackground(Color.DARK_GRAY);
-		btnNewButton_1.setBounds(57, 140, 89, 23);
+		btnNewButton_1.setBounds(283, 189, 89, 23);
 		Rdv.getContentPane().add(btnNewButton_1);
 		
-		JButton btnNewButton_2 = new JButton("M\u00E9decins");
+		JButton btnNewButton_2 = new JButton("Voir");
 		btnNewButton_2.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				Rdv.dispose();
@@ -80,10 +87,10 @@ public class rdv {
 		});
 		btnNewButton_2.setBackground(Color.DARK_GRAY);
 		btnNewButton_2.setForeground(Color.WHITE);
-		btnNewButton_2.setBounds(57, 106, 89, 23);
+		btnNewButton_2.setBounds(283, 106, 89, 23);
 		Rdv.getContentPane().add(btnNewButton_2);
 		
-		JButton btnNewButton_3 = new JButton("Add");
+		JButton btnNewButton_3 = new JButton("Ajouter");
 		btnNewButton_3.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				try {
@@ -99,37 +106,33 @@ public class rdv {
 		});
 		btnNewButton_3.setForeground(Color.WHITE);
 		btnNewButton_3.setBackground(Color.DARK_GRAY);
-		btnNewButton_3.setBounds(57, 72, 89, 23);
+		btnNewButton_3.setBounds(57, 106, 89, 23);
 		Rdv.getContentPane().add(btnNewButton_3);
 		
-		JButton btnNewButton_4 = new JButton("Profil");
-		btnNewButton_4.setBounds(57, 229, 89, 23);
-		Rdv.getContentPane().add(btnNewButton_4);
-		
 		JLabel lblNewLabel = new JLabel("Espace Rendez-vous");
-		lblNewLabel.setFont(new Font("Cambria Math", Font.PLAIN, 13));
-		lblNewLabel.setForeground(Color.WHITE);
-		lblNewLabel.setBounds(47, 47, 139, 14);
+		lblNewLabel.setFont(new Font("Century Gothic", Font.PLAIN, 13));
+		lblNewLabel.setForeground(Color.BLACK);
+		lblNewLabel.setBounds(157, 23, 159, 23);
 		Rdv.getContentPane().add(lblNewLabel);
 		
-		JLabel lblNewLabel_1 = new JLabel(": prendre un rendez-vous");
+		JLabel lblNewLabel_1 = new JLabel("Prendre un rendez-vous :");
 		lblNewLabel_1.setForeground(Color.WHITE);
-		lblNewLabel_1.setBounds(156, 76, 159, 14);
+		lblNewLabel_1.setBounds(35, 81, 159, 14);
 		Rdv.getContentPane().add(lblNewLabel_1);
 		
-		JLabel lblNewLabel_2 = new JLabel(": voir les disponibilit\u00E9s m\u00E9decins");
+		JLabel lblNewLabel_2 = new JLabel("Voir les disponibilit\u00E9s m\u00E9decins :");
 		lblNewLabel_2.setForeground(Color.WHITE);
-		lblNewLabel_2.setBounds(156, 110, 230, 14);
+		lblNewLabel_2.setBounds(253, 81, 177, 14);
 		Rdv.getContentPane().add(lblNewLabel_2);
 		
-		JLabel lblNewLabel_3 = new JLabel(": cr\u00E9er un profil pour patient");
+		JLabel lblNewLabel_3 = new JLabel("Cr\u00E9er un profil pour patient :");
 		lblNewLabel_3.setForeground(Color.WHITE);
-		lblNewLabel_3.setBounds(156, 144, 212, 14);
+		lblNewLabel_3.setBounds(257, 169, 159, 14);
 		Rdv.getContentPane().add(lblNewLabel_3);
 		
-		JLabel lblNewLabel_4 = new JLabel(": afficher les rendez-vous");
+		JLabel lblNewLabel_4 = new JLabel("Afficher les rendez-vous :");
 		lblNewLabel_4.setForeground(Color.WHITE);
-		lblNewLabel_4.setBounds(156, 178, 159, 14);
+		lblNewLabel_4.setBounds(44, 169, 134, 14);
 		Rdv.getContentPane().add(lblNewLabel_4);
 		
 		JButton btnNewButton_5 = new JButton("Retour");
@@ -142,7 +145,7 @@ public class rdv {
 		});
 		btnNewButton_5.setForeground(Color.RED);
 		btnNewButton_5.setBackground(UIManager.getColor("ToolBar.dockingForeground"));
-		btnNewButton_5.setBounds(302, 229, 107, 23);
+		btnNewButton_5.setBounds(157, 230, 107, 23);
 		Rdv.getContentPane().add(btnNewButton_5);
 	}
 
