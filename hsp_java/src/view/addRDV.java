31595124730f7c1f2681e5c78fb6523db8f4d7ff
ryanftne <ryan-fontaine@hotmail.fr>
2021-03-01 15:53:06 +0100

    addRDV fonctionnel

--- hsp_java/src/view/addRDV.java
+++ hsp_java/src/view/addRDV.java
@@ -10,22 +10,29 @@ import java.awt.FlowLayout;
 import java.awt.Color;
 import java.awt.Button;
 import java.awt.event.ActionListener;
+import java.sql.Connection;
+import java.sql.ResultSet;
 import java.awt.event.ActionEvent;
 import javax.swing.JButton;
 import java.awt.SystemColor;
 import javax.swing.JTextField;
 import javax.swing.JSeparator;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JComboBox;
 import com.toedter.calendar.JCalendar;
+
+import db_connexion.DbConnection;
+
 import javax.swing.ImageIcon;
 import java.awt.Font;
 
 public class addRDV {
 
 	JFrame frame;
-	private JTextField textField;
-	private JTextField textField_1;
+	private JTextField txtName;
+	private JTextField txtLastName;
+	private JTextField txtMail;
 
 	/**
 	 * Launch the application.
@@ -60,7 +67,7 @@ public class addRDV {
 		
 		JPanel panel = new JPanel();
 		panel.setBackground(Color.DARK_GRAY);
-		panel.setBounds(0, 0, 426, 487);
+		panel.setBounds(0, 0, 426, 502);
 		frame.getContentPane().add(panel);
 		panel.setLayout(null);
 		
@@ -88,55 +95,136 @@ public class addRDV {
 		});
 		btnBack.setForeground(Color.BLACK);
 		
-		JButton btnNewButton = new JButton("Valider");
-		btnNewButton.setForeground(UIManager.getColor("Button.background"));
-		btnNewButton.setBackground(new Color(241, 57, 83));
-		btnNewButton.setBounds(549, 425, 283, 36);
-		frame.getContentPane().add(btnNewButton);
+		JLabel lblError = new JLabel("");
+		lblError.setBounds(180, 418, 49, 14);
+		panel.add(lblError);
 		
-		textField = new JTextField();
-		textField.setBounds(549, 36, 283, 36);
-		frame.getContentPane().add(textField);
-		textField.setColumns(10);
+		JButton btnNewButton_1 = new JButton("Ajouter un compte");
+		btnNewButton_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				frame.dispose();
+				addProfil window = new addProfil();
+				window.frame.setVisible(true);
+			}
+		});
+		btnNewButton_1.setForeground(Color.WHITE);
+		btnNewButton_1.setBackground(Color.BLUE);
+		btnNewButton_1.setBounds(131, 428, 158, 32);
+		panel.add(btnNewButton_1);
+		
+		txtName = new JTextField();
+		txtName.setBounds(549, 36, 283, 27);
+		frame.getContentPane().add(txtName);
+		txtName.setColumns(10);
 		
 		JLabel lblNewLabel = new JLabel("Nom");
 		lblNewLabel.setBounds(549, 11, 49, 14);
 		frame.getContentPane().add(lblNewLabel);
 		
-		JLabel lblNewLabel_1 = new JLabel("Nom");
-		lblNewLabel_1.setBounds(549, 83, 49, 14);
+		JLabel lblNewLabel_1 = new JLabel("Pr\u00E9nom");
+		lblNewLabel_1.setBounds(549, 74, 49, 14);
 		frame.getContentPane().add(lblNewLabel_1);
 		
-		textField_1 = new JTextField();
-		textField_1.setColumns(10);
-		textField_1.setBounds(549, 105, 283, 36);
-		frame.getContentPane().add(textField_1);
+		txtLastName = new JTextField();
+		txtLastName.setColumns(10);
+		txtLastName.setBounds(549, 93, 283, 27);
+		frame.getContentPane().add(txtLastName);
 		
 		JLabel lblMdecin = new JLabel("M\u00E9decin\r\n");
-		lblMdecin.setBounds(549, 152, 49, 14);
+		lblMdecin.setBounds(549, 190, 49, 14);
 		frame.getContentPane().add(lblMdecin);
 		
 		JLabel lblNewLabel_2_1 = new JLabel("Date");
-		lblNewLabel_2_1.setBounds(549, 212, 49, 14);
+		lblNewLabel_2_1.setBounds(549, 248, 49, 14);
 		frame.getContentPane().add(lblNewLabel_2_1);
 		
-		JComboBox comboBox = new JComboBox();
-		comboBox.setBounds(549, 177, 283, 22);
-		frame.getContentPane().add(comboBox);
+		JComboBox cbMed = new JComboBox();
 		
-		JCalendar calendar = new JCalendar();
-		calendar.setBounds(586, 229, 205, 121);
-		frame.getContentPane().add(calendar);
+		DbConnection Connect = new DbConnection();
+		Connection cnx = Connect.dbConnection();
+		String requete = "Select nom from medecin";
+		ResultSet result = Connect.Requete(cnx, requete);
+
+		try {
+			while(result.next()) {
+				cbMed.addItem(result.getString(1).trim());
+			}
+			result.close();
+		}
+		catch (Exception ex) {System.out.println(ex);}
+		
+		cbMed.setBounds(549, 215, 283, 22);
+		frame.getContentPane().add(cbMed);
+		
+		JCalendar date = new JCalendar();
+		date.setBounds(585, 262, 205, 121);
+		frame.getContentPane().add(date);
 		
 		JLabel lblHeure = new JLabel("Heure");
-		lblHeure.setBounds(549, 361, 49, 14);
+		lblHeure.setBounds(549, 384, 49, 14);
 		frame.getContentPane().add(lblHeure);
 		
-		JComboBox comboBox_1 = new JComboBox();
-		comboBox_1.setBounds(549, 386, 283, 22);
-		frame.getContentPane().add(comboBox_1);
+		
+		JLabel lblNewLabel_1_1 = new JLabel("Mail");
+		lblNewLabel_1_1.setBounds(549, 131, 49, 14);
+		frame.getContentPane().add(lblNewLabel_1_1);
+		
+		txtMail = new JTextField();
+		txtMail.setColumns(10);
+		txtMail.setBounds(549, 152, 283, 27);
+		frame.getContentPane().add(txtMail);
+		
+		JComboBox cbH = new JComboBox();
+		cbH.setBounds(549, 409, 283, 22);
+		frame.getContentPane().add(cbH);
 		frame.setBackground(UIManager.getColor("ToggleButton.highlight"));
 		frame.setBounds(100, 100, 936, 526);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		 requete = "Select time from horaire";
+		 result = Connect.Requete(cnx, requete);
+
+		try {
+			while(result.next()) {
+				cbH.addItem(result.getString(1).trim());
+			}
+			result.close();
+		}
+		catch (Exception ex) {System.out.println(ex);}
+		
+		
+		
+		JButton btnNewButton = new JButton("Valider");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				String requete = "Select date from rdv where date='" + date.getDate() + "'";
+				ResultSet result = Connect.Requete(cnx, requete);
+				try {
+					while(result.next()) {
+						lblError.setText("Le rendez-vous existe déjà!");
+					}
+					requete = "INSERT INTO rdv(nom_patient, prenom_patient, mail_patient, nom_medecin, date, heure) VALUES('" + txtName.getText() + "', '" 
+							+ txtLastName.getText()  + "', '" + txtMail.getText()  + "', '" + cbMed.getSelectedItem().toString()  + "', '" + date.getDate() + "',"
+									+ "'" + cbH.getSelectedItem().toString()  + "')";
+					Connect.Requete_prepare(cnx, requete);
+					
+					JOptionPane.showMessageDialog(null, "Le rendez-vous est confirmé!");
+					
+					frame.dispose();
+					rdv window = new rdv();
+					window.Rdv.setVisible(true);
+
+				}
+				catch (Exception ex) {
+
+				}
+			}
+		});
+		btnNewButton.setForeground(UIManager.getColor("Button.background"));
+		btnNewButton.setBackground(new Color(241, 57, 83));
+		btnNewButton.setBounds(549, 442, 283, 36);
+		frame.getContentPane().add(btnNewButton);
+		
+		
 	}
 }
