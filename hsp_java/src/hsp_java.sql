de985ce876ad260599b21a8434bed3c29d1b6229
ryanftne <ryan-fontaine@hotmail.fr>
2021-03-22 21:30:30 +0100

    Maj bdd

--- hsp_java/src/hsp_java.sql
+++ hsp_java/src/hsp_java.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.5
+-- version 5.0.2
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le :  lun. 15 mars 2021 à 12:45
--- Version du serveur :  5.7.26
--- Version de PHP :  7.2.18
+-- Généré le : lun. 22 mars 2021 à 20:29
+-- Version du serveur :  5.7.31
+-- Version de PHP : 7.3.21
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -19,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de données :  `hsp_java`
+-- Base de données : `hsp_java`
 --
 CREATE DATABASE IF NOT EXISTS `hsp_java` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
 USE `hsp_java`;
@@ -37,10 +36,11 @@ CREATE TABLE IF NOT EXISTS `commande` (
   `fabricant` varchar(50) NOT NULL,
   `qte` int(11) NOT NULL,
   `date` date NOT NULL,
+  `prix` decimal(10,0) NOT NULL,
   PRIMARY KEY (`id`),
   KEY `nom_fabricant` (`fabricant`),
   KEY `nom_medicament` (`nom_medicament`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -72,29 +72,6 @@ INSERT INTO `compte` (`id`, `nom`, `prenom`, `mail`, `mdp`, `acces_rdv`, `acces_
 -- --------------------------------------------------------
 
 --
--- Structure de la table `horaire`
---
-
-DROP TABLE IF EXISTS `horaire`;
-CREATE TABLE IF NOT EXISTS `horaire` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `time` time NOT NULL,
-  `day` int(11) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
-
---
--- Déchargement des données de la table `horaire`
---
-
-INSERT INTO `horaire` (`id`, `time`, `day`) VALUES
-(1, '09:00:00', 0),
-(2, '12:00:00', 0),
-(3, '16:00:00', 0);
-
--- --------------------------------------------------------
-
---
 -- Structure de la table `medecin`
 --
 
@@ -115,7 +92,10 @@ CREATE TABLE IF NOT EXISTS `medecin` (
 INSERT INTO `medecin` (`id`, `nom`, `prenom`, `specialite`) VALUES
 (1, 'Guo', 'Loic', 'Tabacologie'),
 (2, 'Kebiche', 'Killian', 'Neurologue'),
-(4, 'Trafalgar', 'Law', 'Chirurgien');
+(3, 't', 't', 't'),
+(4, 'ah', 'ah', 'ah'),
+(5, 'Fontaine', 'Ryan', 'Bg'),
+(6, 'Trafalgar', 'Law', 'Chirurgien');
 
 -- --------------------------------------------------------
 
@@ -129,25 +109,21 @@ CREATE TABLE IF NOT EXISTS `patient` (
   `nom` varchar(30) NOT NULL,
   `prenom` varchar(30) NOT NULL,
   `mail` varchar(50) NOT NULL,
-  `tel` varchar(10) DEFAULT NULL,
-  `mutuelle` varchar(50) DEFAULT NULL,
-  `secu` varchar(20) DEFAULT NULL,
+  `tel` varchar(10) NOT NULL,
+  `mutuelle` varchar(50) NOT NULL,
+  `secu` int(15) NOT NULL,
   PRIMARY KEY (`id`),
   KEY `nom_patient` (`nom`) USING BTREE,
   KEY `prenom_patient` (`prenom`) USING BTREE,
   KEY `mail_patient` (`mail`)
-) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 
 --
 -- Déchargement des données de la table `patient`
 --
 
 INSERT INTO `patient` (`id`, `nom`, `prenom`, `mail`, `tel`, `mutuelle`, `secu`) VALUES
-(3, 'Nom patient', 'Prénom patient', 'Mail patient', 'Téléphone', 'Mutuelle', 'Numéro de sécu'),
-(5, 'Fontaine', 'Ryan', 'ryan-fontaine@hotmail.fr', '0622560123', 'Sav', '21121332323232'),
-(6, 'Nathan', 'Nathan', 'Nathan', '3232232332', 'Nathan', '3232132132321'),
-(7, '', '', '', '', '', ''),
-(8, 'Test', 'Test', 'Test', '3232323232', 'Test', '32');
+(1, 'Shaco', 'Shaco', 'Shaco', '789789', 'Shaco', 789789789);
 
 -- --------------------------------------------------------
 
@@ -162,26 +138,22 @@ CREATE TABLE IF NOT EXISTS `rdv` (
   `prenom_patient` varchar(30) NOT NULL,
   `mail_patient` varchar(50) NOT NULL,
   `nom_medecin` varchar(30) NOT NULL,
-  `date` varchar(30) NOT NULL,
-  `heure` varchar(8) NOT NULL,
+  `date` date NOT NULL,
+  `heure` time NOT NULL,
   PRIMARY KEY (`id`),
   KEY `nom_patient` (`nom_patient`),
   KEY `prenom_patient` (`prenom_patient`),
   KEY `nom_medecin` (`nom_medecin`),
   KEY `mail_patient` (`mail_patient`)
-) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 
 --
 -- Déchargement des données de la table `rdv`
 --
 
 INSERT INTO `rdv` (`id`, `nom_patient`, `prenom_patient`, `mail_patient`, `nom_medecin`, `date`, `heure`) VALUES
-(1, 'Nathan', 'Nathan', 'Nathan', 'Guo', 'Fri Feb 26 00:00:00 CET 2021', '09:00:00'),
-(2, 'Nathan', 'Nathan', 'Nathan', 'Guo', 'Thu Feb 25 17:10:15 CET 2021', '09:00:00'),
-(6, 'Fontaine', 'Ryan', 'ryan-fontaine@hotmail.fr', 'Guo', 'Mon Mar 01 15:47:43 CET 2021', '09:00:00'),
-(7, 'Fontaine', 'Ryan', 'ryan-fontaine@hotmail.fr', 'Guo', 'Sat Mar 20 15:49:04 CET 2021', '12:00:00'),
-(8, 'Fontaine', 'Ryan', 'ryan-fontaine@hotmail.fr', 'Guo', 'Wed Mar 31 16:16:52 CEST 2021', '09:00:00'),
-(9, 'Fontaine', 'Ryan', 'ryan-fontaine@hotmail.fr', 'Guo', 'Sun Mar 14 16:20:02 CET 2021', '09:00:00');
+(1, 'Shaco', 'Shaco', 'Shaco', 'Guo', '2021-02-10', '15:48:19'),
+(2, 'Shaco', 'Shaco', 'Shaco', 'Kebiche', '2021-02-01', '05:02:14');
 
 -- --------------------------------------------------------
 
@@ -199,21 +171,15 @@ CREATE TABLE IF NOT EXISTS `stock` (
   PRIMARY KEY (`id`),
   KEY `nom_medicament` (`nom`),
   KEY `fabriquant` (`fabricant`)
-) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
 
 --
 -- Déchargement des données de la table `stock`
 --
 
 INSERT INTO `stock` (`id`, `nom`, `fabricant`, `qte`, `danger`) VALUES
-(1, 'Doliprane', 'Sanofi', 5, 'Ne pas prendre en trop grande quantité'),
-(2, 'Spasfon', 'Sanofi', 7, 'Ne pas prendre en trop grande quantité'),
-(3, 'CoDoliprane', 'Roche', 18, 'Euphorie'),
-(5, 'Loicium', 'Guo', 3000, 'Saccisme'),
-(6, 'Opium', 'Freeze', 667, 'Complotiste'),
-(7, 'Codeine', 'Freeze', 667, 'Euphorie'),
-(8, 'Test', 'Test', 50, 'Test'),
-(9, 'Test', 'Test', 50, 'Test');
+(5, 'Loicium', 'M. Guo', 3000, 'Saccisme très important'),
+(6, 'Doliprane', 'Robert Schuman', 20, 'Danger 2');
 
 --
 -- Contraintes pour les tables déchargées
